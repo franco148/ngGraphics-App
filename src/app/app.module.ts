0002080052aeff9a6e439bf3435d1e73846f6ec8
franco-extreme <franck_ral@hotmail.com>
2018-03-29 19:34:14 -0400

    ngGraphics-AppV1: Configuring the project with required dependencies - ng2-charts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
+
+// Charts
+import { ChartsModule } from 'ng2-charts';
+
+// My Components
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
